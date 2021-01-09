80f251797543b98c5654de835e61d735ef6ecdf9
Curtis Mayberry <Curtisma3@gmail.com>
2021-01-09 14:10:50 -0600

    bumped version to 1.0.0

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,6 +1,6 @@
 # Add SKILL_Tools library
 mkdir -p "${PREFIX}/lib/skill/${PKG_NAME}"
-for item in "src" "init.ils" "README.md"; do
+for item in "src" "init.ils" "readme.md"; do
 	cp -rf "${RECIPE_DIR}/../${item}" \
        "${PREFIX}/lib/skill/${PKG_NAME}"
 done
